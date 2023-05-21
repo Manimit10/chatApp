0aee3142bb43b8f75696b81451922ab3ca29bcd6
maniMIT10 <m.tahriri.z@gmail.com>
2023-05-21 20:06:48 +0200

    using prettyChatWindow to make chat page more beautiful

--- frontend/src/ChatPage.jsx
+++ frontend/src/ChatPage.jsx
@@ -1,16 +1,16 @@
 import React from "react"
-import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced'
+import { PrettyChatWindow } from 'react-chat-engine-pretty'
 
 const ChatPage = (props) => {
-    const chatProps = useMultiChatLogic(
-        '4c403451-2ecc-423f-8712-00af8e416ff0', 
-        props.user.username, 
-        props.user.secret
-        );
+    
     return (
         <div style={{ height: '100vh' }}>
-            <MultiChatSocket {...chatProps} />
-            <MultiChatWindow {...chatProps} style={{ height: '100%' }}/>
+            <PrettyChatWindow 
+                projectId="4c403451-2ecc-423f-8712-00af8e416ff0"
+                username=""
+                secret=""
+                style={{ height: '100%'}}
+            />
         </div>
     );
 }
